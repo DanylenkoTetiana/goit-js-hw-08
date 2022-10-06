dddc25911b804ffa045549e647bec913a808fabe
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-06 16:11:54 +0300

    додає слухача на плеєр

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+'user strict';
 
+let getEl = selector => document.querySelector(selector);
+const iframeEl = getEl('#vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+console.log(iframeEl);
+iframeEl.addEventListener('timeupdate', saveTime);
+function saveTime(event) {
+  localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+}
