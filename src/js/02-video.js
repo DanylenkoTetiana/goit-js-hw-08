9c9ce5cf637012f4dc3103470edeae483f5f8c55
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-07 14:25:35 +0300

    додає рішення завдання 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 'user strict';
+import player from '@vimeo/player';
 
 let getEl = selector => document.querySelector(selector);
 const iframeEl = getEl('#vimeo-player');
@@ -8,3 +9,13 @@ iframeEl.addEventListener('timeupdate', saveTime);
 function saveTime(event) {
   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
 }
+console.log(iframeEl.frameborder);
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('timeupdate ', onPlay);
+
+// player.on('timeupdate ', function (data) {
+//   // data is an object containing properties specific to that event
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
+('user strict');
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let getEl = selector => document.querySelector(selector);
+const formEl = getEl('.feedback-form');
+const emailEl = getEl('input');
+const textareaEl = getEl('textarea');
+const formData = {};
+
+updateForm();
+
+formEl.addEventListener('input', throttle(handlerInput, 500));
+formEl.addEventListener('submit', handlerSubmit);
+
+function handlerInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  const informs = localStorage.getItem(LOCALSTORAGE_KEY);
+  console.log(JSON.parse(informs));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function updateForm() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    const informs = localStorage.getItem(LOCALSTORAGE_KEY);
+    const objectValue = JSON.parse(informs);
+    emailEl.value = objectValue.email || '';
+    console.log(emailEl.value);
+    textareaEl.value = objectValue.message || '';
+    console.log(textareaEl.value);
+  }
+}
+
+// function handlerInput(event) {
+//   localStorage.setItem(
+//     LOCALSTORAGE_KEY,
+//     JSON.stringify({ email: emailEl.value, message: textareaEl.value })
+//   );
+// }
