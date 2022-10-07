16139f36d8f874d1068e426201e5139fac82d9e7
DanylenkoTetiana <110232913+DanylenkoTetiana@users.noreply.github.com>
2022-10-07 11:26:16 +0000

    Deploying to gh-pages from @ DanylenkoTetiana/goit-js-hw-08@9c9ce5cf637012f4dc3103470edeae483f5f8c55 🚀

--- 02-video.afbceb01.js
+++
@@ -1,2 +0,0 @@
-const e=(t="#vimeo-player",document.querySelector(t));var t;console.log(e),e.addEventListener("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",form.elements.message.value)}));
-//# sourceMappingURL=02-video.afbceb01.js.map
--- 02-video.fe78f40a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t=(e="#vimeo-player",document.querySelector(e));console.log(t),t.addEventListener("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",form.elements.message.value)}))}();
-//# sourceMappingURL=02-video.fe78f40a.js.map
